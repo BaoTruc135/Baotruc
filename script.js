b4e214d98d2cc1cc7d8a4178186756692df45101
BaoTruc135 <vungocbaotruc@gmail.com>
2025-02-21 10:19:09 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function login()
+{
+  let u = username.value
+  let p = pw.value
+  if (u=="truc" && p=="bi")
+  {
+    window.open("home.html")
+    document getElementById(compiler).style.display="block"
+  }
+}
